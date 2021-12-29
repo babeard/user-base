6d910d250ac5314f691f9531af3ffd5aa6940748
Brian Beard <brian.a.beard@gmail.com>
2021-12-28 21:35:09 -0600

    add change password

--- src/modules/user/ConfirmUser.ts
+++ src/modules/user/ConfirmUser.ts
@@ -1,4 +1,5 @@
 import { Arg, Ctx, Mutation, Resolver } from "type-graphql";
+import { confirmUserPrefix } from "../../constants/redisPrefixes";
 import { User } from "../../entity/User";
 import { redis } from "../../redis";
 import { MyContext } from "../../types/MyContext";
@@ -6,14 +7,14 @@ import { MyContext } from "../../types/MyContext";
 @Resolver()
 export class ConfirmUserResolver {
   @Mutation(() => Boolean)
-  async confirmUser(@Arg("token") token: string, @Ctx() ctx: MyContext) {
-    const userId = await redis.get(token);
+  async confirmUser(@Arg("token") token: string) {
+    const userId = await redis.get(confirmUserPrefix + token);
 
     if (!userId) return false;
 
     await User.update({ id: +userId }, { confirmed: 1 });
 
-    await redis.del(token);
+    await redis.del(confirmUserPrefix + token);
 
     return true;
   }
--- src/utils/createConfirmationUrl.ts
+++ src/utils/createConfirmationUrl.ts
@@ -1,9 +1,10 @@
 import { v4 } from "uuid";
+import { confirmUserPrefix } from "../constants/redisPrefixes";
 import { redis } from "../redis";
 
 export const createConfirmationUrl = async (userId: number) => {
   const token = v4();
-  await redis.set(token, userId, "ex", 60 * 60 * 24);
+  await redis.set(confirmUserPrefix + token, userId, "ex", 60 * 60 * 24);
 
   return `http://localhost:3000/confirm/${token}`;
 };
